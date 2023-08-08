4650b1b0898d0d44bc8188937805bcea67c006d4
hjha <hjha@gaia3d.com>
2023-08-08 13:57:55 +0900

    feat: querydsl 설정과 검증 관련 코드 추가

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,35 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	@PersistenceContext
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello fetchOne = query
+			.selectFrom(qHello)
+			.fetchOne();
+
+		Assertions.assertThat(fetchOne).isEqualTo(hello);
 	}
 
 }
