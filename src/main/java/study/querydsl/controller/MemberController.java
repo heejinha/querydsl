86306f9695728a85285c8c34095f000b4d6f8c12
hjha <hjha@gaia3d.com>
2023-08-23 22:48:35 +0900

    feat: [64affef] rest api 추가

---
+++ src/main/java/study/querydsl/controller/MemberController.java
@@ -0,0 +1,26 @@
+package study.querydsl.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.repository.MemberJPARepository;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberJPARepository memberJPARepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJPARepository.search(condition);
+    }
+    
+}
